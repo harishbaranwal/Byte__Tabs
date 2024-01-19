78c2181bd80ad3a34de76891c76dcca52565e7ce
harishbaranwal <114431658+harishbaranwal@users.noreply.github.com>
2024-01-20 00:10:45 +0530

    Update about.js

--- about.js
+++ about.js
@@ -1,5 +1,25 @@
 
-function closeNav() {
+function openNav() {
+    document.getElementById("mySidenav").style.width = "150px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  document.querySelector('.hamburger').addEventListener('click', function() {
+    console.log('Hamburger clicked');
+    openNav();
+  });
+  
+  
+  function openNav() {
+    var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var sidenavWidth = (screenWidth < 400) ? "100px" : "150px";
+    document.getElementById("mySidenav").style.width = sidenavWidth;
+  }
+  
+  
+  function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
   document.querySelector('.hamburger').addEventListener('click', function() {
@@ -25,3 +45,25 @@ function closeNav() {
   slideShow();
   
   
+  
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+  
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+  });
+  
+  window.addEventListener('scroll', function () {
+    var scrollPosition = window.scrollY;
+    document.querySelectorAll('a[href^="#"]').forEach(function (el) {
+        var section = document.querySelector(el.getAttribute('href'));
+        if (section.offsetTop <= scrollPosition && section.offsetTop + section.offsetHeight > scrollPosition) {
+            el.classList.add('active');
+        } else {
+            el.classList.remove('active');
+        }
+    });
+  });
